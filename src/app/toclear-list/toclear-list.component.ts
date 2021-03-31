f624601cf2d6d4013d0341594d023f66925ec079
Atchukorn Tonsiriskulvong <[tatchukorn@gmail.com]>
2021-03-31 15:51:36 +0700

    text

--- src/app/toclear-list/toclear-list.component.ts
+++ src/app/toclear-list/toclear-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Task } from '../task';
+import { TaskComponent } from '../task/task.component';
 
 @Component({
   selector: 'app-toclear-list',
@@ -6,19 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toclear-list.component.css']
 })
 export class ToclearListComponent {
+ @ViewChildren(TaskComponent) taskComps!: QueryList<TaskComponent>;
 
-  task:string[] = [ ];
 
-  drink!: string;
+  task:Task[] = [ ];
+
+  taskName!: string;
+  taskDescription!: string;
+
+  choose!: Task;
 
   addtask(){
-    this.task.push(this.drink);
+    let tempTask: Task = {
+      id : this.UUID(),
+      name: this.taskName,
+      description: this.taskDescription,
+    }
+    this.task.push(tempTask);
   }
 
-  //  deletedtask(clearlist:string){
-    // this.task=this.task.filter((k)=> k !==clearlist);
-    deletedtask(drinkIndex:number){
-      this.task=this.task.filter((k, index)=> index !==drinkIndex);
-
+  deletedtask(TaskComponent:TaskComponent){
+    this.task=this.task.filter(t => t.id !== TaskComponent.taskObj.id);
+  }
+  UUID(){
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+  selectedTask(TaskComponent:TaskComponent){
+      this.clearSelected();
+        TaskComponent.isSelected =true;
+        this.choose = TaskComponent.taskObj;
+      }
+  clearSelected() {
+    this.taskComps.forEach(task =>{
+     task.isSelected = false;
+    });
   }
 }
